8aad50857a14fd357ee7c2c1f183cb55c0f319d1
Sharan <sharanshyamsundar@gmail.com>
2025-01-22 23:46:11 +0100

    Integreated all Agents

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import random
 import time
 import json
 from utils import ai_greetings, is_url, is_sql_statement
-from agents import TaskDetectingAgent, SemanticSearch, EasyNavAgent, SQLXMLGenAgent
+from agents import TaskDetectingAgent, SemanticSearch, EasyNavAgent, SQLXMLGenAgent, SQLtoXML
 from dotenv import dotenv_values
 from openai import OpenAI
 
@@ -30,16 +30,33 @@ def initialize_env_variables():
     
     if "tool_check" not in st.session_state:
         st.session_state.tool_check = None
+    
+    if 'xml_data' not in st.session_state:
+        st.session_state['xml_data'] = {}
+    
+    if 'xml_step' not in st.session_state:
+        st.session_state['xml_step'] = 0
+        
+    if 'generating_xml' not in st.session_state:
+        st.session_state['generating_xml'] = {}
+        
+    if 'generated_query' not in st.session_state:
+        st.session_state['generated_query'] = None
 
 initialize_env_variables()
 
 def new_chat():
+    
     st.session_state.task_detector_agent = TaskDetectingAgent(st.session_state.client)
     st.session_state.search_system = SemanticSearch()
     st.session_state.easy_nav_agent = EasyNavAgent(st.session_state.client, st.session_state.search_system)
     st.session_state.sql_xml_gen_agent = SQLXMLGenAgent(st.session_state.client)
     st.session_state.tool_counter = 0
     st.session_state.tool_check = None
+    st.session_state.xml_data = {}
+    st.session_state.generating_xml = False
+    st.session_state.xml_step = 0
+    st.session_state.generated_query = None
     
     st.session_state.messages = [
         st.session_state.task_detector_agent.task_detector_convo[0],
@@ -142,39 +159,77 @@ if st.session_state.messages[-1]["role"] not in ["assistant","tool"]:
             elif function_name == "sql_generator":
                 print()
                 print(f"Entering Function:{function_name}")    
-                # print("===========")
-                # llm_response = task_classifier(prompt, st.session_state.messages, config["OPENAI_KEY"])
-                # sql_generator(prompt)
                 
-                with st.spinner("Generating SQL query..."):
+                if not st.session_state.generating_xml:
                     
-                    assistant_response = {
-                        "role":"assistant",
-                        "content":f"Calling Function",
-                        "tool_calls": [{"id":st.session_state.tool_check.message.tool_calls[0].id,
-                                        "type":"function",
-                                        "function":{"arguments":prompt,
-                                                    "name":function_name}}]
-                    }
-                    st.session_state.messages.append(assistant_response)
+                    with st.spinner("Generating SQL query..."):
                     
-                    response = st.session_state.sql_xml_gen_agent.generate_response(prompt)
-                    st.markdown(response)
+                        assistant_response = {
+                            "role":"assistant",
+                            "content":f"Calling Function",
+                            "tool_calls": [{"id":st.session_state.tool_check.message.tool_calls[0].id,
+                                            "type":"function",
+                                            "function":{"arguments":prompt,
+                                                        "name":function_name}}]
+                        }
+                        st.session_state.messages.append(assistant_response)
                     
-                    # print("response:",response)
-                    tool_response = {
-                        "role": "tool",
-                        "content": response,
-                        "tool_call_id": st.session_state.tool_check.message.tool_calls[0].id,
-                        "name": function_name
-                    }
-                    st.session_state.messages.append(tool_response)
+                        response = st.session_state.sql_xml_gen_agent.generate_response(prompt)
+                        st.markdown(response)
                     
-                    if is_sql_statement(response):
-                        print("generating xml")
-                        st.session_state.tool_counter = 0
+                        # print("response:",response)
+                        tool_response = {
+                            "role": "tool",
+                            "content": response,
+                            "tool_call_id": st.session_state.tool_check.message.tool_calls[0].id,
+                            "name": function_name
+                        }
+                        st.session_state.messages.append(tool_response)
+                        
+                        query_to_check = str(response).replace("```","").replace("sql","")
+                        
+                        if is_sql_statement(query_to_check):
+                            print("SQL Done")
+                            st.session_state.generated_query = query_to_check         
+                            print("Lets Generate XML")
+                            st.session_state.generating_xml = True
+                            if st.session_state.xml_step == 0:
+                                question = "To generate an XML, Please give a name for this new view"
+                                st.markdown(question)
+                                st.session_state.messages.append({"role":"assistant", "content": question})
+                                st.session_state.xml_step += 1
+                                
+                    
+                else:
+                    with st.spinner("Generating XML"):    
+                        if st.session_state.xml_step == 1:
+                            st.session_state['xml_data']['viewName'] = prompt
+                            question = "Please tell me what does this view mean"
+                            st.markdown(question)
+                            st.session_state.messages.append({"role":"assistant", "content": question})
+                            st.session_state.xml_step += 1
+                        else:
+                            st.session_state['xml_data']['viewDesc'] = prompt
+                            filename = str(st.session_state['xml_data']['viewName'])
+                            with open(f"config/{filename}.sql", 'w') as f:
+                                f.write(str(st.session_state.generated_query))
+                                
+                            with open('config/view.json', 'w') as json_file:
+                                json.dump(st.session_state['xml_data'], json_file)
+                                
+                                
+                            xml_creator = SQLtoXML(st.session_state.client)
+                            xml_creator.convert_sql_to_xml()
+                            st.markdown("XML Successfully Generated")
+                            st.session_state.messages.append({"role":"assistant", "content": "XML Successfully Generated"})
+                                                        
+                            st.session_state.generating_xml = False
+                            st.session_state.xml_step = 0
+                            st.session_state['xml_data'] = {}
+                            st.session_state.tool_counter = 0
+                            st.session_state.generated_query = ""
 
-                        # Can you write sql query to merge table LMF1 and LMF2
+                        # Can you write sql query to merge table LFM1 and LFM2
                         # Inner join on both columns
                     
                     # print("Printing response in main")
